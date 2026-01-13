eclipse-rdf4j/rdf4j

GH-5327 added RDF 1.2 testsuite files for Turtle, TriG, NTriples, NQuads, and refactored testsuite code to run RDF 1.2 and 1.1 test suites

compliance/rio/src/test/java/org/eclipse/rdf4j/rio/nquads/NQuadsParserTest.java
@@ -10,24 +10,50 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.rio.nquads;
 
-import org.eclipse.rdf4j.rio.RDFParser;
-import org.eclipse.rdf4j.testsuite.rio.nquads.AbstractNQuadsParserTest;
-
 import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParser;
+import org.eclipse.rdf4j.testsuite.rio.AbstractParserTestSuite;
 
-/**
- * JUnit test for the N-Quads parser.
- *
- * @author Arjohn Kampman
- */
-public class NQuadsParserTest extends AbstractNQuadsParserTest {
+public class NQuadsParserTest {
 
 	public static Test suite() throws Exception {
-		return new NQuadsParserTest().createTestSuite();
+		final TestSuite suite = new TestSuite(NQuadsParserTest.class);
+		suite.addTest(NQuads11ParserTest.suite());
+		suite.addTest(NQuads12ParserTest.suite());
+		return suite;
 	}
 
-	@Override
-	protected RDFParser createRDFParser() {
-		return new NQuadsParser();
+	static class NQuads11ParserTest extends AbstractParserTestSuite {
+
+		private NQuads11ParserTest() {
+			super("/testcases/nquads/rdf11/", "http://www.w3.org/2013/N-QuadsTests/", RDFFormat.NQUADS, "NQuads");
+		}
+
+		public static Test suite() throws Exception {
+			return new NQuads11ParserTest().createTestSuite();
+		}
+
+		@Override
+		protected RDFParser createRDFParser() {
+			return new NQuadsParser();
+		}
+	}
+
+	static class NQuads12ParserTest extends AbstractParserTestSuite {
+
+		private NQuads12ParserTest() {
+			super("/testcases/nquads/rdf12/", "http://www.w3.org/2013/N-QuadsTests/", RDFFormat.NQUADS, "NQuads");
+		}
+
+		public static Test suite() throws Exception {
+			return new NQuads12ParserTest().createTestSuite();
+		}
+
+		@Override
+		protected RDFParser createRDFParser() {
+			return new NQuadsParser();
+		}
 	}
 }

compliance/rio/src/test/java/org/eclipse/rdf4j/rio/trig/TriGParserTest.java
@@ -10,29 +10,67 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.rio.trig;
 
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.rio.helpers.BasicParserSettings;
 import org.eclipse.rdf4j.rio.nquads.NQuadsParser;
-import org.eclipse.rdf4j.testsuite.rio.trig.TriGParserTestCase;
+import org.eclipse.rdf4j.testsuite.rio.AbstractParserTestSuite;
 
-/**
- * JUnit test for the TriG parser.
- */
-public class TriGParserTest extends TriGParserTestCase {
+public class TriGParserTest {
 
-	public static junit.framework.Test suite() throws Exception {
-		return new TriGParserTest().createTestSuite();
+	public static Test suite() throws Exception {
+		final TestSuite suite = new TestSuite(TriGParserTest.class);
+		suite.addTest(TriG11ParserTest.suite());
+		suite.addTest(TriG12ParserTest.suite());
+		return suite;
 	}
 
-	@Override
-	protected RDFParser createTriGParser() {
-		TriGParser parser = new TriGParser();
-		parser.set(BasicParserSettings.VERIFY_DATATYPE_VALUES, true);
-		return parser;
+	static class TriG11ParserTest extends AbstractParserTestSuite {
+		protected static final String TESTS_W3C_BASE_URL = "http://www.w3.org/2013/TriGTests/";
+		protected static final String TEST_W3C_FILE_BASE_PATH = "/testcases/trig/rdf11/";
+
+		public TriG11ParserTest() {
+			super(TEST_W3C_FILE_BASE_PATH, TESTS_W3C_BASE_URL, RDFFormat.TRIG, "Trig");
+		}
+
+		public static Test suite() throws Exception {
+			return new TriG11ParserTest().createTestSuite();
+		}
+
+		@Override
+		protected RDFParser createRDFParser() {
+			return new TriGParser().set(BasicParserSettings.VERIFY_DATATYPE_VALUES, true);
+		}
+
+		@Override
+		protected RDFParser createRDFBaseParser() {
+			return new NQuadsParser();
+		}
 	}
 
-	@Override
-	protected RDFParser createNQuadsParser() {
-		return new NQuadsParser();
+	static class TriG12ParserTest extends AbstractParserTestSuite {
+		protected static final String TESTS_W3C_BASE_URL = "https://w3c.github.io/rdf-tests/rdf/rdf12/rdf-trig/";
+		protected static final String TEST_W3C_FILE_BASE_PATH = "/testcases/trig/rdf12/";
+
+		public TriG12ParserTest() {
+			super(TEST_W3C_FILE_BASE_PATH, TESTS_W3C_BASE_URL, RDFFormat.TRIG, "Trig");
+		}
+
+		public static Test suite() throws Exception {
+			return new TriG12ParserTest().createTestSuite();
+		}
+
+		@Override
+		protected RDFParser createRDFParser() {
+			return new TriGParser().set(BasicParserSettings.VERIFY_DATATYPE_VALUES, true);
+		}
+
+		@Override
+		protected RDFParser createRDFBaseParser() {
+			return new NQuadsParser();
+		}
 	}
 }
+

compliance/rio/src/test/java/org/eclipse/rdf4j/rio/turtle/TurtleParserTest.java
@@ -10,32 +10,66 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.rio.turtle;
 
-import org.eclipse.rdf4j.rio.RDFParser;
-import org.eclipse.rdf4j.rio.helpers.BasicParserSettings;
-import org.eclipse.rdf4j.rio.ntriples.NTriplesParser;
-import org.eclipse.rdf4j.testsuite.rio.turtle.TurtleParserTestCase;
-
 import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParser;
+import org.eclipse.rdf4j.rio.ntriples.NTriplesParser;
+import org.eclipse.rdf4j.testsuite.rio.AbstractParserTestSuite;
 
-/**
- * JUnit test for the Turtle parser that uses the tests that are available
- * <a href="https://dvcs.w3.org/hg/rdf/file/09a9da374a9f/rdf-turtle/">online</a>.
- */
-public class TurtleParserTest extends TurtleParserTestCase {
+public class TurtleParserTest {
 
 	public static Test suite() throws Exception {
-		return new TurtleParserTest().createTestSuite();
+		final TestSuite suite = new TestSuite(TurtleParserTest.class);
+		suite.addTest(Turtle11ParserTest.suite());
+		suite.addTest(Turtle12ParserTest.suite());
+		return suite;
 	}
 
-	@Override
-	protected RDFParser createTurtleParser() {
-		RDFParser result = new TurtleParser();
-		result.set(BasicParserSettings.VERIFY_DATATYPE_VALUES, true);
-		return result;
+	static class Turtle12ParserTest extends AbstractParserTestSuite {
+
+		protected static final String TESTS_W3C_BASE_URL = "https://w3c.github.io/rdf-tests/rdf/rdf12/rdf-turtle/";
+		protected static final String TEST_W3C_FILE_BASE_PATH_RDF12 = "/testcases/turtle/rdf12/";
+
+		private Turtle12ParserTest() {
+			super(TEST_W3C_FILE_BASE_PATH_RDF12, TESTS_W3C_BASE_URL, RDFFormat.TURTLE, "Turtle");
+		}
+
+		public static Test suite() throws Exception {
+			return new Turtle12ParserTest().createTestSuite();
+		}
+
+		@Override
+		protected RDFParser createRDFParser() {
+			return new TurtleParser();
+		}
+
+		@Override
+		protected RDFParser createRDFBaseParser() {
+			return new NTriplesParser();
+		}
 	}
 
-	@Override
-	protected RDFParser createNTriplesParser() {
-		return new NTriplesParser();
+	static class Turtle11ParserTest extends AbstractParserTestSuite {
+		protected static final String TESTS_W3C_BASE_URL = "https://w3c.github.io/rdf-tests/rdf/rdf11/rdf-turtle/";
+		protected static final String TEST_W3C_FILE_BASE_PATH = "/testcases/turtle/rdf11/";
+
+		public Turtle11ParserTest() {
+			super(TEST_W3C_FILE_BASE_PATH, TESTS_W3C_BASE_URL, RDFFormat.TURTLE, "Turtle");
+		}
+
+		public static Test suite() throws Exception {
+			return new Turtle11ParserTest().createTestSuite();
+		}
+
+		@Override
+		protected RDFParser createRDFParser() {
+			return new TurtleParser();
+		}
+
+		@Override
+		protected RDFParser createRDFBaseParser() {
+			return new NTriplesParser();
+		}
 	}
 }

Ontotext-AD/rdf4j-jakarta

compliance/rio/src/test/java/org/eclipse/rdf4j/rio/nquads/NQuadsParserTest.java
@@ -10,24 +10,33 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.rio.nquads;
 
-import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.testsuite.rio.nquads.AbstractNQuadsParserTest;
 
 import junit.framework.Test;
+import junit.framework.TestSuite;
 
-/**
- * JUnit test for the N-Quads parser.
- *
- * @author Arjohn Kampman
- */
-public class NQuadsParserTest extends AbstractNQuadsParserTest {
+public class NQuadsParserTest {
 
 	public static Test suite() throws Exception {
-		return new NQuadsParserTest().createTestSuite();
+		final TestSuite suite = new TestSuite();
+		suite.addTest(NQuads11ParserTest.suite());
+//		suite.addTest(NQuads12ParserTest.suite());
+		return suite;
 	}
 
-	@Override
-	protected RDFParser createRDFParser() {
-		return new NQuadsParser();
+	static class NQuads11ParserTest extends AbstractNQuadsParserTest {
+		public static Test suite() throws Exception {
+			return new NQuads11ParserTest().createTestSuite();
+		}
+	}
+
+	static class NQuads12ParserTest extends AbstractNQuadsParserTest {
+		private NQuads12ParserTest() {
+			super("/testcases/nquads/rdf12/", "http://www.w3.org/2013/N-QuadsTests/");
+		}
+
+		public static Test suite() throws Exception {
+			return new NQuads12ParserTest().createTestSuite();
+		}
 	}
 }

compliance/rio/src/test/java/org/eclipse/rdf4j/rio/trig/TriGParserTest.java
@@ -10,29 +10,36 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.rio.trig;
 
-import org.eclipse.rdf4j.rio.RDFParser;
-import org.eclipse.rdf4j.rio.helpers.BasicParserSettings;
-import org.eclipse.rdf4j.rio.nquads.NQuadsParser;
 import org.eclipse.rdf4j.testsuite.rio.trig.TriGParserTestCase;
 
-/**
- * JUnit test for the TriG parser.
- */
-public class TriGParserTest extends TriGParserTestCase {
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
-	public static junit.framework.Test suite() throws Exception {
-		return new TriGParserTest().createTestSuite();
+public class TriGParserTest {
+
+	public static Test suite() throws Exception {
+		final TestSuite suite = new TestSuite();
+		suite.addTest(TriG11ParserTest.suite());
+//		suite.addTest(TriG12ParserTest.suite());
+		return suite;
 	}
 
-	@Override
-	protected RDFParser createTriGParser() {
-		TriGParser parser = new TriGParser();
-		parser.set(BasicParserSettings.VERIFY_DATATYPE_VALUES, true);
-		return parser;
+	static class TriG11ParserTest extends TriGParserTestCase {
+		public static Test suite() throws Exception {
+			return new TriG11ParserTest().createTestSuite();
+		}
 	}
 
-	@Override
-	protected RDFParser createNQuadsParser() {
-		return new NQuadsParser();
+	static class TriG12ParserTest extends TriGParserTestCase {
+		protected static final String TESTS_W3C_BASE_URL = "https://w3c.github.io/rdf-tests/rdf/rdf12/rdf-trig/";
+		protected static final String TEST_W3C_FILE_BASE_PATH = "/testcases/trig/rdf12/";
+
+		public TriG12ParserTest() {
+			super(TEST_W3C_FILE_BASE_PATH, TESTS_W3C_BASE_URL);
+		}
+
+		public static Test suite() throws Exception {
+			return new TriG12ParserTest().createTestSuite();
+		}
 	}
 }

compliance/rio/src/test/java/org/eclipse/rdf4j/rio/turtle/TurtleParserTest.java
@@ -10,32 +10,36 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.rio.turtle;
 
-import org.eclipse.rdf4j.rio.RDFParser;
-import org.eclipse.rdf4j.rio.helpers.BasicParserSettings;
-import org.eclipse.rdf4j.rio.ntriples.NTriplesParser;
 import org.eclipse.rdf4j.testsuite.rio.turtle.TurtleParserTestCase;
 
 import junit.framework.Test;
+import junit.framework.TestSuite;
 
-/**
- * JUnit test for the Turtle parser that uses the tests that are available
- * <a href="https://dvcs.w3.org/hg/rdf/file/09a9da374a9f/rdf-turtle/">online</a>.
- */
-public class TurtleParserTest extends TurtleParserTestCase {
+public class TurtleParserTest {
 
 	public static Test suite() throws Exception {
-		return new TurtleParserTest().createTestSuite();
+		final TestSuite suite = new TestSuite();
+		suite.addTest(Turtle11ParserTest.suite());
+//		suite.addTest(Turtle12ParserTest.suite());
+		return suite;
 	}
 
-	@Override
-	protected RDFParser createTurtleParser() {
-		RDFParser result = new TurtleParser();
-		result.set(BasicParserSettings.VERIFY_DATATYPE_VALUES, true);
-		return result;
+	static class Turtle11ParserTest extends TurtleParserTestCase {
+		public static Test suite() throws Exception {
+			return new Turtle11ParserTest().createTestSuite();
+		}
 	}
 
-	@Override
-	protected RDFParser createNTriplesParser() {
-		return new NTriplesParser();
+	static class Turtle12ParserTest extends TurtleParserTestCase {
+		protected static final String TESTS_W3C_BASE_URL = "https://w3c.github.io/rdf-tests/rdf/rdf12/rdf-turtle/";
+		protected static final String TEST_W3C_FILE_BASE_PATH_RDF12 = "/testcases/turtle/rdf12/";
+
+		private Turtle12ParserTest() {
+			super(TEST_W3C_FILE_BASE_PATH_RDF12, TESTS_W3C_BASE_URL);
+		}
+
+		public static Test suite() throws Exception {
+			return new Turtle12ParserTest().createTestSuite();
+		}
 	}
 }